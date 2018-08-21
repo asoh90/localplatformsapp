0d26adf6bda4e10f8580ef32993f233ae0fcc797
Alvin Soh <alvinsoh@Alvins-MacBook-Pro.local>
2018-08-21 23:07:15 +0800

    Added lots of stuff and worked on forms

--- home.py
+++ home.py
@@ -1,25 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect
+from forms import SelectPlatformForm, SelectFunctionForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'a1ac33ec538de1e200d5f537e717ae6b'
 
 PLATFORMS = [
     {
         'display_name':'AppNexus',
-        'value':'appnexus'
     },
     {
         'display_name':'The Trade Desk',
-        'value':'ttd'
     }
 ]
 
-@app.route("/")
-@app.route("/home")
+@app.route("/", methods=['GET','POST'])
+@app.route("/home", methods=['GET','POST'])
 def home():
-    return render_template('home.html', posts=PLATFORMS)
+    form = SelectPlatformForm()
+    if form.validate_on_submit():
+        flash(f'Platform selected: {form.platform.data}','success')
+        return redirect(url_for('function'))
+    return render_template('home.html', form=form, posts=PLATFORMS)
 
-@app.route("/about")
-def about():
-    return render_template('about.html', title="About")
+@app.route("/function", methods=['GET','POST'])
+def function():
+    form = SelectFunctionForm()
+    return render_template('function.html', title="Select Function")
 
 if __name__ == "__main__":
     app.run()
