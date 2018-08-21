3808a6e9023a6d1bfc0588d08d3f36e9c9b38913
Alvin Soh <alvinsoh@Alvins-MacBook-Pro.local>
2018-08-21 17:00:49 +0800

    edited home.py

--- home.py
+++ home.py
@@ -2,8 +2,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/home")
+def home():
+    return "<h1>Hello World!</h1>"
+
+@app.route("/about")
+def about():
+    return "<h2>About Page</h2>"
 
 if __name__ == "__main__":
     app.run()
