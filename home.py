fd7e76df47d7554ce54c4492a74f46293de6b896
Alvin Soh <alvinsoh@Alvins-MacBook-Pro.local>
2018-08-21 18:58:15 +0800

    created block codes

--- home.py
+++ home.py
@@ -1,14 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+PLATFORMS = [
+    {
+        'display_name':'AppNexus',
+        'value':'appnexus'
+    },
+    {
+        'display_name':'The Trade Desk',
+        'value':'ttd'
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Hello World!</h1>"
+    return render_template('home.html', posts=PLATFORMS)
 
 @app.route("/about")
 def about():
-    return "<h2>About Page</h2>"
+    return render_template('about.html', title="About")
 
 if __name__ == "__main__":
     app.run()
