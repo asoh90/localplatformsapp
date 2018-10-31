20c647b4afd4223a9dafe19fbeb23a2762a62e5b
Alvin Soh <alvinsoh@Alvins-MacBook-Pro.local>
2018-10-31 19:57:20 +0800

    added authentication for mediamath

--- platforms/mediamath.py
+++ platforms/mediamath.py
@@ -1,27 +1,56 @@
-from requests_oauthlib import OAuth2Session
-from oauthlib.oauth2 import BackendApplicationClient
-import variables
+import requests
 
 # Authenticate credentials
-URL = "https://auth.mediamath.com/"
-AUTHENTICATE_URL = URL + "oauth/token"
-AUDIENCE = "https://api.mediamath.com/"
+AUTHENTICATE_URL = "https://auth.mediamath.com/oauth/token"
+URL = "https://api.mediamath.com/"
+API_URL = URL + "api/v2.0/"
+SESSION_URL = API_URL + "session"
 
 CLIENT_ID = "IBxiUniDVrRYSdSXUHJgoq6KdJ7F5oN0"
 CLIENT_SECRET = "NnU9qtfRtruQypo7e2QJh_as_HjlDjppZAhBP0wWeRkqdSzcVrZSln_8PdXrOn50"
+
+
 
 def callAPI(platform, function, file_path):
     if function == "Test":
         authenticate()
 
 def authenticate():
-    client = BackendApplicationClient(client_id=CLIENT_ID)
-    oauth = OAuth2Session(client=client)
-    print(variables.login_credentials)
     username = variables.login_credentials['MediaMath']['Login']
     password = variables.login_credentials['MediaMath']['PW']
-    token = oauth.fetch_token(token_url=AUTHENTICATE_URL, username=username, password=password, client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
-    print(token)
-    return token
+    access_token = None
 
-# token = authenticate()
+    auth_request = requests.post(AUTHENTICATE_URL,
+                        headers={
+                                'Content-Type':'application/json'
+                        },
+                        json={
+                              'grant_type':'password',
+                              'username':username,
+                              'password':password,
+                              'audience':URL,
+                              'scope':"",
+                              'client_id':CLIENT_ID,
+                              'client_secret':CLIENT_SECRET
+                        })
+    print("Authenticate URL: {}".format(auth_request.url))
+
+    if auth_request.status_code == 200:
+        auth_response = auth_request.json()
+        access_token = auth_response["access_token"]
+    return access_token
+
+def get_session(access_token):
+    adama_session = None
+    session_request = requests.get(SESSION_URL,
+                        headers={
+                            'Authorization':'Bearer ' + access_token,
+                        })
+    print("Session URL: {}".format(session_request.url))
+
+    if session_request.status_code == 200:
+        session_cookies = session_request.cookies
+        session_dict = session_cookies.get_dict()
+        adama_session = session_dict["adama_session"]
+
+    return adama_session
