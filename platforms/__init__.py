02162a8eb739824f174fea92f950c8dd8b46c72d
Alvin Soh <alvinsoh@alvins-mbp.lan>
2019-06-18 16:22:55 +0800

    added adobeadcloud

--- platforms/__init__.py
+++ platforms/__init__.py
@@ -3,5 +3,6 @@ import platforms.appnexus
 import platforms.yahoo
 import platforms.mediamath
 import platforms.adobeaam
+import platforms.adobeadcloud
 import platforms.adform
 import platforms.all_report_platforms
