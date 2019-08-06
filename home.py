5f9ac3625691a2a4282645d2e8b94b340eb5f8e9
Alvin Soh <alvinsoh@Alvins-MacBook-Pro.local>
2019-08-06 20:45:18 +0800

    add validation for email

--- home.py
+++ home.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
-from flask import Flask, render_template, url_for, flash, redirect, request, send_file
+from flask import Flask, render_template, url_for, flash, redirect, request, send_file, session
+from flask_oauth import OAuth
 from forms import SelectPlatformForm, SelectFunctionForm
 from werkzeug.utils import secure_filename
 import variables
@@ -11,17 +12,106 @@ import sys
 import datetime
 import logging
 from logging.handlers import RotatingFileHandler
+from urllib.request import Request, urlopen, URLError
+import json
+
+# Google SSO Credentials
+GOOGLE_CLIENT_ID = '696774976262-rg17k58uiani498vqkjdekfdunh7c6j3.apps.googleusercontent.com'
+GOOGLE_CLIENT_SECRET = 'ceM7QWDLzwA-7xJ0qalIPweP'
+REDIRECT_URI = "/oauth2callback"
+DEBUG = True
 
 app = Flask(__name__)
+# app.debug = DEBUG
+oauth = OAuth()
 
 app.config['SECRET_KEY'] = variables.SECRET_KEY
 app.config["UPLOAD_FOLDER"] = variables.UPLOAD_FOLDER
 RETURN_FOLDER = variables.RETURN_FOLDER
 UPLOAD_FOLDER = variables.UPLOAD_FOLDER
 
+google = oauth.remote_app('google',
+                          base_url='https://www.google.com/accounts/',
+                          authorize_url='https://accounts.google.com/o/oauth2/auth',
+                          request_token_url=None,
+                          request_token_params={'scope': 'https://www.googleapis.com/auth/userinfo.email',
+                                                'response_type': 'code'},
+                          access_token_url='https://accounts.google.com/o/oauth2/token',
+                          access_token_method='POST',
+                          access_token_params={'grant_type': 'authorization_code'},
+                          consumer_key=GOOGLE_CLIENT_ID,
+                          consumer_secret=GOOGLE_CLIENT_SECRET)
+
 platform_functions = variables.platform_functions
 
-@app.route("/", methods=['GET','POST'])
+@app.route("/")
+def index():
+    # access_token = session.get('access_token')
+    # # print("ACCESS TOKEN: {}".format(access_token))
+    # if access_token is None:
+    #     return redirect(url_for('login'))
+    # access_token = access_token[0]
+    
+    # headers = {'Authorization': 'OAuth '+ access_token}
+    # req = Request('https://www.googleapis.com/oauth2/v1/userinfo',
+    #               None, headers)
+    # try:
+    #     res = urlopen(req)
+    # except URLError as e:
+    #     if e.code == 401:
+    #         # Unauthorized - bad token
+    #         session.pop('access_token', None)
+    #         return redirect(url_for('login'))
+
+    # login_response = res.read().decode('utf-8')
+    # login_dict = json.loads(login_response)
+    # session["email"] = login_dict["email"]
+    check_output = check_login()
+    if check_output == None:
+        return redirect(url_for('home'))
+    else:
+        return check_output
+
+def check_login():
+    access_token = session.get('access_token')
+    # print("ACCESS TOKEN: {}".format(access_token))
+    if access_token is None:
+        return redirect(url_for('login'))
+    access_token = access_token[0]
+    
+    headers = {'Authorization': 'OAuth '+ access_token}
+    req = Request('https://www.googleapis.com/oauth2/v1/userinfo',
+                  None, headers)
+    try:
+        res = urlopen(req)
+    except URLError as e:
+        if e.code == 401:
+            # Unauthorized - bad token
+            session.pop('access_token', None)
+            return redirect(url_for('login'))
+
+    login_response = res.read().decode('utf-8')
+    login_dict = json.loads(login_response)
+    session["email"] = login_dict["email"]
+    return None
+ 
+@app.route('/login')
+def login():
+    callback=url_for('authorized', _external=True)
+    return google.authorize(callback=callback)
+
+@app.route(REDIRECT_URI)
+@google.authorized_handler
+def authorized(resp):
+    access_token = resp['access_token']
+    session['access_token'] = access_token, ''
+    return redirect(url_for('index'))
+
+@google.tokengetter
+def get_access_token():
+    return session.get('access_token')
+
+@app.route("/home", methods=['GET','POST'])
 def home():
     # delete all files in upload and to_return folders
     delete_upload_and_to_return_files()
